63d3fc4308a7dfd3ba9a6b6892e416bc0e7f91f9
Cristian Yepes <ycristian@uninorte.edu.co>
2021-03-18 11:22:52 -0500

    added login and chatroom front

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,12 +2,22 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//= link_tree ../images
+//= link_directory ../stylesheets .css
+//= require semantic-ui
+//= require turbolinks
+//= require jquery
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+$(document).on('turbolinks:load', function() {
+    $('.ui.dropdown').dropdown();
+})
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
